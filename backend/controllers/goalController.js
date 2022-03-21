f75b22ec059cb08dec63d54fba1496ac80e39ca6
George <georgegaibor@gmail.com>
2022-03-21 12:58:57 -0500

    Initial REST API for goals

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,5 +1,5 @@
 const asyncHandler = require('express-async-handler');
-
+const Goal = require('../models/goalModel')
 //CREATE
 //@desc     Set goals
 //@route    POST /api/goals
@@ -11,9 +11,11 @@ const setGoal = asyncHandler(async (req,res) => {
         throw new Error('Please add a text field');
     }
     
-    res.status(200).send({
-        message: 'Set goal'
+    const goal = await Goal.create({
+        text: req.body.text,
     });
+
+    res.status(200).send(goal);
 });
 
 //READ
@@ -21,9 +23,8 @@ const setGoal = asyncHandler(async (req,res) => {
 //@route    GET /api/goals
 //@access   Private
 const getGoal = asyncHandler(async (req,res)=>{
-    res.status(200).json({
-        message:'Get goal'
-    });
+    const goals = await Goal.find();
+    res.status(200).json(goals);
 });
 
 
@@ -32,9 +33,16 @@ const getGoal = asyncHandler(async (req,res)=>{
 //@route    PUT /api/goals/:id
 //@access   Private
 const updateGoal = asyncHandler(async (req,res)=>{
-    res.status(200).json({
-        message: `Update goal${req.params.id}`
-    });
+    const goal = await Goal.findById(req.params.id);
+
+    if(!goal){
+        res.status(400); 
+        throw new Error('Goal not found');
+    }
+
+    const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {new: true});
+
+    res.status(200).json(updatedGoal);
 });
 
 //DELETE
@@ -42,9 +50,16 @@ const updateGoal = asyncHandler(async (req,res)=>{
 //@route    DELETE /api/goals/:id
 //@access   Private
 const deleteGoal = asyncHandler(async (req,res)=>{
-    res.status(200).json({
-        message: `Delete goal${req.params.id}`
-    });
+    const goal = await Goal.findById(req.params.id);
+    
+    if(!goal){
+        res.status(400); 
+        throw new Error('Goal not found');
+    }
+
+    await goal.remove();
+
+    res.status(200).json({id: req.params.id});
 });
 
 
